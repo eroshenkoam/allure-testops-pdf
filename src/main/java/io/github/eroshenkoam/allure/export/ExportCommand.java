1f336c50b8205b43010fa848864758b9af71bbae
Artem Eroshenko <eroshenkoam@me.com>
2021-02-04 15:20:43 +0300

    add search and template rft (via #1)

--- src/main/java/io/github/eroshenkoam/allure/export/ExportCommand.java
+++ src/main/java/io/github/eroshenkoam/allure/export/ExportCommand.java
@@ -1,12 +1,15 @@
 package io.github.eroshenkoam.allure.export;
 
+import io.github.eroshenkoam.allure.dto.ReportMeta;
 import io.github.eroshenkoam.allure.dto.ResultTree;
 import io.github.eroshenkoam.allure.dto.ResultTreeGroup;
 import io.github.eroshenkoam.allure.dto.ResultTreeLeaf;
 import io.github.eroshenkoam.allure.utils.FreemarkerUtils;
 import io.github.eroshenkoam.allure.utils.PDFUtils;
+import io.qameta.allure.ee.client.LaunchService;
 import io.qameta.allure.ee.client.ServiceBuilder;
 import io.qameta.allure.ee.client.TestResultTreeService;
+import io.qameta.allure.ee.client.dto.Launch;
 import io.qameta.allure.ee.client.dto.Page;
 import io.qameta.allure.ee.client.dto.TestResultTreeGroup;
 import io.qameta.allure.ee.client.dto.TestResultTreeLeaf;
@@ -16,9 +19,11 @@ import retrofit2.Response;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @CommandLine.Command(
@@ -41,6 +46,12 @@ public class ExportCommand implements Runnable {
     protected String token;
 
     @CommandLine.Option(
+            names = {"--name"},
+            description = "Report name"
+    )
+    protected String name;
+
+    @CommandLine.Option(
             names = {"--launch-id"},
             description = "Allure TestOps launch id"
     )
@@ -51,6 +62,12 @@ public class ExportCommand implements Runnable {
             description = "Allure TestOps tree id"
     )
     protected Long treeId;
+
+    @CommandLine.Option(
+            names = {"--search"},
+            description = "Test Result search filter"
+    )
+    protected String search;
 
     @CommandLine.Option(
             names = {"--output-file"},
@@ -70,34 +87,54 @@ public class ExportCommand implements Runnable {
 
     private void runUnsafe() throws Exception {
         final ServiceBuilder builder = new ServiceBuilder(endpoint).authToken(token);
+        final Map<String, Object> parameters = new HashMap<>();
+
+        final LaunchService launchService = builder.create(LaunchService.class);
+        final ReportMeta meta = getReportData(launchId, launchService);
+        parameters.put("meta", meta);
 
         final TestResultTreeService testResultTreeService = builder.create(TestResultTreeService.class);
-        final ResultTree tree = getTree(launchId, treeId, testResultTreeService);
-
-        final Map<String, Object> parameters = new HashMap<>();
+        final ResultTree tree = getTree(launchId, treeId, search, testResultTreeService);
         parameters.put("tree", tree);
 
         final String html = FreemarkerUtils.render("report.ftl", parameters);
         PDFUtils.saveToFile(html, outputPath.toString());
     }
 
+    private ReportMeta getReportData(final Long launchId,
+                                     final LaunchService service) throws IOException {
+        final Response<Launch> launchResponse = service.findById(launchId).execute();
+        if (!launchResponse.isSuccessful()) {
+            throw new RuntimeException(launchResponse.message());
+        }
+        final Launch launch =  launchResponse.body();
+        final String metaName = Optional.ofNullable(this.name)
+                .orElse(launch.getName());
+
+        return new ReportMeta()
+                .setName(metaName)
+                .setCreatedDate(new Date(launch.getCreatedDate()));
+    }
+
     private ResultTree getTree(final Long launchId,
                                final Long treeId,
+                               final String search,
                                final TestResultTreeService service) throws IOException {
         final ResultTree tree = new ResultTree();
-        tree.setGroups(getChildGroups(launchId, treeId, new ArrayList<>(), service));
-        tree.setLeaves(getChildLeaves(launchId, treeId, new ArrayList<>(), service));
+        tree.setGroups(getChildGroups(launchId, treeId, new ArrayList<>(), search, service));
+        tree.setLeaves(getChildLeaves(launchId, treeId, new ArrayList<>(), search, service));
         return tree;
     }
 
     private List<ResultTreeGroup> getChildGroups(final Long launchId,
                                                  final Long treeId,
                                                  final List<Long> paths,
+                                                 final String search,
                                                  final TestResultTreeService service) throws IOException {
         final List<ResultTreeGroup> groups = new ArrayList<>();
 
         final Response<Page<TestResultTreeGroup>> groupResponse = service
-                .getGroups(launchId, treeId, paths, 0, 1000).execute();
+                .getGroups(launchId, treeId, paths, search, 0, 1000).execute();
         if (!groupResponse.isSuccessful()) {
             throw new RuntimeException(groupResponse.message());
         }
@@ -113,8 +150,8 @@ public class ExportCommand implements Runnable {
             final List<Long> childPath = new ArrayList<>(paths);
             childPath.add(responseGroup.getId());
 
-            group.setGroups(getChildGroups(launchId, treeId, childPath, service));
-            group.setLeaves(getChildLeaves(launchId, treeId, childPath, service));
+            group.setGroups(getChildGroups(launchId, treeId, childPath, search, service));
+            group.setLeaves(getChildLeaves(launchId, treeId, childPath, search, service));
 
             groups.add(group);
         }
@@ -125,9 +162,10 @@ public class ExportCommand implements Runnable {
     private List<ResultTreeLeaf> getChildLeaves(final Long launchId,
                                                 final Long treeId,
                                                 final List<Long> paths,
+                                                final String search,
                                                 final TestResultTreeService service) throws IOException {
         final Response<Page<TestResultTreeLeaf>> leavesResponse = service
-                .getLeafs(launchId, treeId, paths, 0, 1000).execute();
+                .getLeafs(launchId, treeId, paths, search, 0, 1000).execute();
         if (!leavesResponse.isSuccessful()) {
             throw new RuntimeException(leavesResponse.message());
         }
